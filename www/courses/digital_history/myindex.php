6fbbd20b70f7c5722fc1e1b6a34b4742668d2aff
sergey <sgolitsynskiy@gmail.com>
2016-03-21 09:23:01 -0500

    content edit

--- www/courses/digital_history/myindex.php
+++ www/courses/digital_history/myindex.php
@@ -1,4 +1,4 @@
-<p>This is the future web site of the HIST/COMM 4159/5159: Digital History course, offered next Fall 2016 and taught by <a href="http://lcarrchilders.com">Dr. Leisl Carr Childers</a> and <a href="http://sergey.cs.uni.edu/">Sergey Golitsynskiy</a>. 
+<p>This is the future web site of the HIST/COMM 4159/5159: Digital History course, offered next Fall 2016 and taught by <a href="http://lcarrchilders.com">Leisl Carr Childers</a> and <a href="http://sergey.cs.uni.edu/">Sergey Golitsynskiy</a>. 
 <p>We will be adding content to this site over the summer. For now, <a href="flyer.pdf">here's the course announcement</a>.
 <p>If you have any questions about the course, please feel free contact us at <a href="mailto:leisl.carr-childers@uni.edu">leisl.carr-childers@uni.edu</a> or <a href="mailto:sergey@uni.edu">sergey@uni.edu</a>. 
 <p>We hope to see you next Fall!
