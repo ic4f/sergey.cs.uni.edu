e656ae06e8bc992d370c9a85daf98c8948c92131
sergey <sgolitsynskiy@gmail.com>
2016-08-17 22:50:02 -0500

    editing comm2555:

--- www/courses/comm2555/fall2016/myindex.php
+++ www/courses/comm2555/fall2016/myindex.php
@@ -1,4 +1,31 @@
+<div style="overflow:auto;float:left;">
+<img src="images/textbook_williams.jpg" alt="" 
+    class="textbook" style="width:100px;">
+<img src="images/textbook_shay.jpg" alt="" class="textbook" style="width: 100px;">
+</div>
+
 <h3>About the Course</h3>
+
+<aside class="aside1">
+    <h5>Links</h5>
+    <ul>
+        <li><a href="syllabus-cs1120-spring2016.pdf">Syllabus</a>
+        <li><a href="resources">Resources</a>
+        <li><a href="https://bb9.uni.edu/">eLearning</a>
+        <li><a href="https://cs.uni.edu/">cs.uni.edu</a>
+        <li><a href="credits">Credits</a>
+    </ul>
+</aside>
+
+<p>CS 1120: Media Computation provides an introduction to computation, algorithmic thinking, data transformation and processing, and programming in the context of media such as images, sound, and video (course description from the university catalog). 
+
+<p>This course has two primary goals. First, you will study digital media and some of the techniques that are used to digitally represent photo, sound, and text files. Second, the course aims to teach you a bit about how to write computer programs. We hope that you leave this course with a sense of what programs can do and how you can write programs to express ideas. 
+<p>Media computation is the primary theme in this course. Computing with images, sound, and text offers a rich body of problems to solve that will bring us into contact with many of the fundamental ideas of computer science: representing and transforming data; design, analysis, and experimentation; and the thrill of solving problems in any domain of human thought. 
+
+
+
+<br clear="all">
+<h3>Course Schedule</h3>
 <table border=1 class="schedule days-2">
 	<thead>
 		<tr>
@@ -10,234 +37,202 @@
 			<th>Homework
 	<tbody>
 		<tr>
-			<td>1</td>
+			<td rowspan="2">1</td>
 			<td>Tue, 08/23
 			<td>
-			<td>
+			<td>Session 1: Introduction to the course
 			<td>
 			<td>
 		<tr>
-			<td>1</td>
 			<td>Thu, 08/25
 			<td>
-			<td>
-			<td>
-			<td>
+			<td>Session 2: How the Internet works, text editors and coding, file management and FTP
+			<td>Lab 1: file management, FTP
+			<td>Homework 1
 		<tr>
-			<td>2</td>
+			<td rowspan="2">2</td>
 			<td>Tue, 08/30
 			<td>
-			<td>
+			<td>Session 3: Introducing HTML and CSS
 			<td>
 			<td>
 		<tr>
-			<td>2</td>
 			<td>Thu, 09/01
 			<td>
-			<td>
-			<td>
+			<td>Session 4: HTML: common elements; structure and semantics
+			<td>Lab 2: basic HTML
 			<td>
 		<tr>
-			<td>3</td>
+			<td rowspan="2">3</td>
 			<td>Tue, 09/06
 			<td>
+			<td>Session 5: CSS common properties: color and length; basic selectors; cascading and inheritance
 			<td>
-			<td>
-			<td>
+			<td>Homework 2
 		<tr>
-			<td>3</td>
 			<td>Thu, 09/08
 			<td>
-			<td>
-			<td>
-			<td>
+			<td>Session 6: CSS: styling text
+            <td>Lab 3: basic CSS
+            <td>
 		<tr>
-			<td>4</td>
+			<td rowspan="2">4</td>
 			<td>Tue, 09/13
 			<td>
-			<td>
+			<td>Session 7: HTML structured content: lists and tables
 			<td>
 			<td>
 		<tr>
-			<td>4</td>
 			<td>Thu, 09/15
 			<td>
-			<td>
-			<td>
+			<td>Session 8: CSS: styling lists and tables
+			<td>Lab 4: lists, tables
 			<td>
 		<tr>
-			<td>5</td>
+			<td rowspan="2">5</td>
 			<td>Tue, 09/20
 			<td>
+			<td>Session 9: HTML: media and links
 			<td>
-			<td>
-			<td>
+			<td>Homework 3
 		<tr>
-			<td>5</td>
 			<td>Thu, 09/22
 			<td>
-			<td>
-			<td>
+			<td>Session 10: CSS box model
+			<td>Lab 5: media, links
 			<td>
 		<tr>
-			<td>6</td>
+			<td rowspan="2">6</td>
 			<td>Tue, 09/27
 			<td>
-			<td>
+			<td>Session 11: CSS positioning: normal flow, relative, absolute, static
 			<td>
 			<td>
 		<tr>
-			<td>6</td>
 			<td>Thu, 09/29
 			<td>
-			<td>
-			<td>
+			<td>Session 12: CSS floats and reusable layouts
+			<td>Lab 6: CSS positioning and floats
 			<td>
 		<tr>
-			<td>7</td>
+			<td rowspan="2">7</td>
 			<td>Tue, 10/04
 			<td>
+			<td>Session 13: CSS gradients and transitions
 			<td>
-			<td>
-			<td>
+			<td>Homework 4
 		<tr>
-			<td>7</td>
 			<td>Thu, 10/06
 			<td>
-			<td>
-			<td>
+			<td>Session 14: exam review
+			<td>Lab 7: exam practice
 			<td>
 		<tr>
-			<td>8</td>
+			<td rowspan="2">8</td>
 			<td>Tue, 10/11
-			<td>
-			<td>
-			<td>
-			<td>
+			<td colspan="4">Exam-1
 		<tr>
-			<td>8</td>
 			<td>Thu, 10/13
 			<td>
-			<td>
+			<td>Session 15: copyright; Creative Commons
 			<td>
 			<td>
 		<tr>
-			<td>9</td>
+			<td rowspan="2">9</td>
 			<td>Tue, 10/18
 			<td>
-			<td>
+			<td>Session 16: design principles: proximity and alignment
 			<td>
 			<td>
 		<tr>
-			<td>9</td>
 			<td>Thu, 10/20
 			<td>
-			<td>
-			<td>
+			<td>Session 17: introduction to Photoshop; selection tools
+			<td>Lab 8: Photoshop practice
 			<td>
 		<tr>
-			<td>10</td>
+			<td rowspan="2">10</td>
 			<td>Tue, 10/25
 			<td>
+			<td>Session 18: design principles: repetition and contrast; Illustrator
 			<td>
-			<td>
-			<td>
+			<td>Homework 5
 		<tr>
-			<td>10</td>
 			<td>Thu, 10/27
 			<td>
-			<td>
-			<td>
+			<td>Session 19: .....
+			<td>Lab 9: Illustrator practice
 			<td>
 		<tr>
-			<td>11</td>
+			<td rowspan="2">11</td>
 			<td>Tue, 11/01
 			<td>
-			<td>
+			<td>Session 20: Designing with color
 			<td>
 			<td>
 		<tr>
-			<td>11</td>
 			<td>Thu, 11/03
 			<td>
-			<td>
-			<td>
+			<td>Session 21: Photoshop adjustment layers
+			<td>Lab 10: Photoshop practice
 			<td>
 		<tr>
-			<td>12</td>
+			<td rowspan="2">12</td>
 			<td>Tue, 11/08
 			<td>
+			<td>Session 22: 
 			<td>
-			<td>
-			<td>
+			<td>Homework 6
 		<tr>
-			<td>12</td>
 			<td>Thu, 11/10
 			<td>
-			<td>
-			<td>
+			<td>Session 23:
+			<td>Lab 11: 
 			<td>
 		<tr>
-			<td>13</td>
+			<td rowspan="2">13</td>
 			<td>Tue, 11/15
-			<td>
-			<td>
-			<td>
-			<td>
+			<td colspan="4">Exam-2
 		<tr>
-			<td>13</td>
 			<td>Thu, 11/17
 			<td>
-			<td>
+			<td>Session 24:
 			<td>
 			<td>
 		<tr class="break">
-			<td>14</td>
+			<td rowspan="2">14</td>
 			<td>Tue, 11/22
-			<td>THANKSGIVING BREAK
-			<td>
-			<td>
-			<td>
+			<td colspan="4" rowspan="2">THANKSGIVING BREAK
 		<tr class="break">
-			<td>14</td>
 			<td>Thu, 11/24
-			<td>
-			<td>
-			<td>
-			<td>
 		<tr>
-			<td>15</td>
+			<td rowspan="2">15</td>
 			<td>Tue, 11/29
 			<td>
-			<td>
+			<td>Session 25:
 			<td>
 			<td>
 		<tr>
-			<td>15</td>
 			<td>Thu, 12/01
 			<td>
-			<td>
+			<td>Session 26:
 			<td>
 			<td>
 		<tr>
-			<td>16</td>
+			<td rowspan="2">16</td>
 			<td>Tue, 12/06
 			<td>
-			<td>
+			<td>Session 27:
 			<td>
 			<td>
 		<tr>
-			<td>16</td>
 			<td>Thu, 12/08
 			<td>
-			<td>
+			<td>Session 28:
 			<td>
 			<td>
 		<tr>
 			<td>17</td>
 			<td>Tue, 12/13
-			<td>
-			<td>
-			<td>
-			<td>
+			<td colspan="4">Final Project Presentation
 </table>
