8eeeae4eeae583a679540b729d033575c05c335f
sergey <sgolitsynskiy@gmail.com>
2016-09-09 21:17:34 -0500

    fixed html

--- www/courses/cs1100/fall2016/sessions/3.php
+++ www/courses/cs1100/fall2016/sessions/3.php
@@ -8,8 +8,7 @@
     <li><a href="#5">Debugging and validating your code</a>
 </ol>
 
-<a name="1"></a>
-<h4>1. What is a markup language</h4>
+<h4 id="1">1. What is a markup language</h4>
 <p>HTML stands for <b>H</b>yper<b>t</b>ext <b>M</b>arkup <b>L</b>anguage. Markup is used to annotate text. A markup <em>language</em> is used to annotate text in a systematic way. In the world of print, markup could be editing instructions added to a manuscript by an editor. In school, markup could be the corrections a teacher makes to a student's paper. In a web page, markup is instructions on how to display the content (or, more precisely, a description of that content's structure). 
 
 <p>In other words, markup is information about the content.
@@ -27,8 +26,7 @@
 
 <p>Thus, HTML is a formal language used for describing the structure of a web page, that is understood by the browser.
 
-<a name="2"></a>
-<h4>2. HTML: versions, browser wars, standards movement</h4>
+<h4 id="2">2. HTML: versions, browser wars, standards movement</h4>
 <ul>
     <li>1991: first web page goes online
     <li>1997: The W3C releases the specification of HTML 4.0 as a W3C Recommendation 
@@ -54,8 +52,7 @@
         </ul>
 </ul>
 
-<a name="3"></a>
-<h4>3. HTML syntax </h4>
+<h4 id="3">3. HTML syntax </h4>
 <p>Besides the material offered by your textbook, there are lots of HTML tutorials and other resources available online. 
 <ul>
     <li><a href="https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Introduction">Here's a very brief overview of HTML</a>  
@@ -83,7 +80,7 @@
 
 <pre class="code">&lt;tag&gt; 
 
-&lt;tag&gt;some content</tag>
+&lt;tag&gt;some content&lt;/tag&gt;
 
 &lt;tag attribute="value"&gt;
 
@@ -95,8 +92,7 @@
 <p>There are lots of HTML elements, and you should not try to learn them all. As you construct more and more web pages, you will come to know more and more HTML elements, many of which you will eventually remember.  But there's no need to memorize them on purpose. There are many references available online. When you come across an element you don't know - look it up! 
 <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element">Here's an excellent resource</a>.
 
-<a name="4"></a>
-<h4>4. Structure of an HTML document</h4>
+<h4 id="4">4. Structure of an HTML document</h4>
 <p>An HTML document consists of 2 main parts: the head and the body:
 <ul>
     <li>the <b>HEAD</b> contains information about the web page (not displayed by the browser)
@@ -124,8 +120,7 @@
 
 <p>However, my <strong>strong</strong> recommendation is to include three tags of these elements. Code is written to be read by humans; that includes others who will read your code, and you - for you will be reading (and editing) it over and over again while developing your web site. Separating the head (with information about your content) from the actual content makes coding much easier! In other words, keep the meta data separate from the data!
 
-<a name="5"></a>
-<h4>5. Debugging and validating your code</h4>
+<h4 id="5">5. Debugging and validating your code</h4>
 
 <p>Here's <a href="https://developer.mozilla.org/en-US/docs/Learn/HTML/Introduction_to_HTML/Debugging_HTML">a good set of guidelines on how to debug your HTML</a>.
 
