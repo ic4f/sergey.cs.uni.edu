9f4a89dd89204ab905303630f8f7fa7849be3aac
sergey <sgolitsynskiy@gmail.com>
2016-09-13 10:50:11 -0500

    resources

--- www/courses/comm2555/fall2016/resources.php
+++ www/courses/comm2555/fall2016/resources.php
@@ -4,7 +4,15 @@
     <li><em>The Non-Designer's Design Book</em> (4th Edition) by Robin Williams. ISBN-13: 978-0-13-396615-2
     <li><em>Learn to Code HTML & CSS: Develop and Style Websites</em> by Shay Howe. ISBN-13: 978-0321940520
 </ul>
+
 <h4>Software</h4>
 
 <p>Link to Adobe Creative Cloud student discount: <a href="https://creative.adobe.com/plans?plan=edu&promoid=KTROQ">https://creative.adobe.com/plans?plan=edu&promoid=KTROQ</a>
 
+<p>Suggested text editors: <a href="https://notepad-plus-plus.org/">Notepad++</a>, <a href="http://brackets.io/">Brackets</a> or TextEdit (the default Mac editor). However, there are <a href="https://en.wikipedia.org/wiki/List_of_text_editors">lots of great text editors</a> out there. If you are enjoy writing code, pick a serious text editor and learn to use it very well.  
+
+<p>Suggested FTP clients: <a href="https://filezilla-project.org/">FileZilla</a>, <a href="https://winscp.net">WinSCP</a> or <a href="https://cyberduck.io">Cyberduck</a>. 
+
+<h4>Code</h4>
+
+<p><a href="code/template.html">Blank HTML template</a> (right-click, then save link as)
