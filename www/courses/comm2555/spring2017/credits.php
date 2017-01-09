a085f9eb21c7da9d5ce8039a7b3bfb5e792c6a5d
sergey <sgolitsynskiy@gmail.com>
2017-01-08 21:43:36 -0600

    comm2555 completed

--- www/courses/comm2555/spring2017/credits.php
+++
@@ -1,5 +0,0 @@
-<p>
-
-
-This course was initially created by <a href="http://www.uni.edu/fabos/">Dr. Bettina Fabos</a> as the foundational course of the <a href="http://ids.uni.edu">Interactive Digital Studies</a> (IDS) program. We keep updating and improving this course every semester. Many, if not most of the course materials used on this website have been created collaboratively. 
-
--- www/courses/comm2555/spring2017/myindex.php
+++ www/courses/comm2555/spring2017/myindex.php
@@ -5,11 +5,10 @@
 <aside class="aside1">
     <h5>Links</h5>
     <ul>
-        <li><a href="syllabus-comm2555-fall2016.pdf">Syllabus</a>
+        <li><a href="syllabus-comm2555-spring2017.pdf">Syllabus</a>
         <li><a href="resources">Resources</a>
         <li><a href="https://bb9.uni.edu/">eLearning</a>
         <li><a href="http://ids.uni.edu/">ids.uni.edu</a>
-        <li><a href="credits">Credits</a>
     </ul>
 </aside>
 
@@ -37,17 +36,17 @@
 			<td rowspan="3">1
 			<td>Mon, 01/09
 			<td>
-			<td>
+			<td>Introduction to the course
 			<td>
 		<tr>
 			<td>Wed, 01/11
-			<td>
-			<td>
+			<td>Duckett, Intro
+			<td>Introduction to the Internet and the WWW
 			<td>
 		<tr>
 			<td>Fri, 01/13
-			<td>
-			<td>Lab 1
+			<td>Duckett, ch. 1
+			<td>Building web pages with HTML
 			<td>
 		<tr class="week break">
 			<td rowspan="3" style="background:#f1f1f1;">2
@@ -58,22 +57,22 @@
 			<td>Wed, 01/18
 			<td>
 			<td>NO CLASS
-			<td>
+			<td>Assignment 1
 		<tr>
 			<td>Fri, 01/20
 			<td>
-			<td>
+			<td>Lab 1
 			<td>
 		<tr class="week">
 			<td rowspan="3">3
 			<td>Mon, 01/23
-			<td>
-			<td>
+			<td>Duckett, ch. 2
+			<td>HTML common elements; structure and semantics
 			<td>
 		<tr>
 			<td>Wed, 01/25
-			<td>
-			<td>
+			<td>Duckett, ch. 3, 6
+			<td>HTML structured content: lists and tables
 			<td>
 		<tr>
 			<td>Fri, 01/27
@@ -83,13 +82,13 @@
 		<tr class="week">
 			<td rowspan="3">4
 			<td>Mon, 01/30
-			<td>
-			<td>
-			<td>
+			<td>Duckett, ch. 4
+			<td>HTML links
+			<td>Assignment 2
 		<tr>
 			<td>Wed, 02/01
-			<td>
-			<td>
+			<td>Duckett, ch. 5
+			<td>HTML images
 			<td>
 		<tr>
 			<td>Fri, 02/03
@@ -99,13 +98,13 @@
 		<tr class="week">
 			<td rowspan="3">5
 			<td>Mon, 02/06
-			<td>
-			<td>
+			<td>Duckett, 8, 10
+			<td>Styling web pages with CSS
 			<td>
 		<tr>
 			<td>Wed, 02/08
 			<td>
-			<td>
+			<td>CSS: how the cascade works; common selectors
 			<td>
 		<tr>
 			<td>Fri, 02/10
@@ -115,13 +114,13 @@
 		<tr class="week">
 			<td rowspan="3">6
 			<td>Mon, 02/13
-			<td>
-			<td>
-			<td>
+			<td>Duckett, ch. 11
+			<td>CSS: styling color
+			<td>Assignment 3
 		<tr>
 			<td>Wed, 02/15
-			<td>
-			<td>
+			<td>Duckett, ch. 12
+			<td>CSS: styling text
 			<td>
 		<tr>
 			<td>Fri, 02/17
@@ -131,50 +130,50 @@
 		<tr class="week">
 			<td rowspan="3">7
 			<td>Mon, 02/20
-			<td>
-			<td>
+			<td>Duckett, ch. 13
+			<td>CSS box model
 			<td>
 		<tr>
 			<td>Wed, 02/22
-			<td>
-			<td>
+			<td>Duckett, ch. 14
+			<td>CSS: styling tables and lists
 			<td>
 		<tr>
 			<td>Fri, 02/24
 			<td>
-			<td>Exam review
+			<td>Lab 6
 			<td>
 		<tr class="week">
 			<td rowspan="3">8
 			<td>Mon, 02/27
-			<td>
-			<td>Exam 1
-			<td>
+			<td>Duckett, ch. 15
+			<td>CSS layout: positioning, floats
+			<td>Assignment 4 
 		<tr> 
 			<td>Wed, 03/01
-			<td>
-			<td>
+			<td>Duckett, ch. 16
+			<td>CSS layout: images; basic multi-column layouts
 			<td>
 		<tr>
 			<td>Fri, 03/03
 			<td>
-			<td>Lab 6
+			<td>Lab 7
 			<td>
 		<tr class="week">
 			<td rowspan="3">9
 			<td>Mon, 03/06
 			<td>
-			<td>
+			<td>Exam 1
 			<td>
 		<tr>
 			<td>Wed, 03/08
-			<td>
-			<td>
+			<td><a href="http://www.nytimes.com/2006/06/26/arts/26crea.html">In Digital Age...</a>
+			<td>Copyright, Public Domain, and the Creative Commons
 			<td>
 		<tr>
 			<td>Fri, 03/10
-			<td>
-			<td>Lab 7
+			<td>Lynda tutorials
+			<td>Lab 8: Photoshop layers and basic selection techniques
 			<td>
 
 		<tr class="break">
@@ -188,103 +187,108 @@
 		<tr class="week">
 			<td rowspan="3">10
 			<td>Mon, 03/20
-			<td>
-			<td>
+			<td>Williams, ch. 1, 2, 3
+			<td>Design principles: proximity and alignment 
 			<td>
 		<tr>
 			<td>Wed, 03/22
-			<td>
-			<td>
+			<td>Lynda tutorials
+			<td>Photoshop compositing, more selection techniques, smart objects
 			<td>
 		<tr>
 			<td>Fri, 03/24
 			<td>
-			<td>Lab 8
-			<td>
+			<td>Lab 9: Photoshop practice; CSS layout
+			<td>Assignment 5
 		<tr class="week">
 			<td rowspan="3">11
 			<td>Mon, 03/27
-			<td>
-			<td>
+			<td>Williams, ch. 4, 5
+			<td>Design principles: repetition and contrast
 			<td>
 		<tr>
 			<td>Wed, 03/29
-			<td>
-			<td>
+			<td>Lynda tutorials
+			<td>Photoshop adjustment layers
 			<td>
 		<tr>
 			<td>Fri, 03/31
 			<td>
-			<td>Exam review
+			<td>Lab 10: Photoshop practice; CSS color and gradients
 			<td>
 		<tr class="week">
 			<td rowspan="3">12
 			<td>Mon, 04/03
-			<td>
-			<td>Exam 2
-			<td>
+			<td>Williams, ch. 7
+			<td>Designing with color
+			<td>Assignment 6
 		<tr>
 			<td>Wed, 04/05
-			<td>
-			<td>
+			<td>Williams, ch. 10-12
+			<td>Typography
 			<td>
 		<tr>
 			<td>Fri, 04/07
 			<td>
-			<td>Lab 9
+			<td>Lab 11
 			<td>
 		<tr class="week">
 			<td rowspan="3">13
 			<td>Mon, 04/10
 			<td>
-			<td>
+			<td>Exam 2
 			<td>
 		<tr>
 			<td>Wed, 04/12
-			<td>
-			<td>
+			<td>TBD
+			<td>Building web sites: web page structure
 			<td>
 		<tr>
 			<td>Fri, 04/14
 			<td>
-			<td>Lab 10
+			<td>Lab 12
 			<td>
 		<tr class="week">
 			<td rowspan="3">14
 			<td>Mon, 04/17
-			<td>
-			<td>
-			<td>
+			<td>TBD
+			<td>CSS: approaches to layout
+			<td>Assignment 7
 		<tr>
 			<td>Wed, 04/19
-			<td>
-			<td>
+			<td>TBD
+			<td>CSS: responsive design 
 			<td>
 		<tr>
 			<td>Fri, 04/21
 			<td>
-			<td>Lab 11
+			<td>Lab 13
 			<td>
 		<tr class="week">
 			<td rowspan="3">15
 			<td>Mon, 04/24
-			<td>
-			<td>
+			<td>TBD
+			<td>Project implementation: layout
 			<td>
 		<tr>
 			<td>Wed, 04/26
-			<td>
-			<td>
+			<td>TBD
+			<td>Project implementation: editorial style
 			<td>
 		<tr>
 			<td>Fri, 04/28
 			<td>
-			<td>Course review
+			<td>Project implementation: wrap-up
 			<td>
 		<tr class="week">
-			<td>16
+			<td rowspan="2">16
 			<td>Mon, 05/01
 			<td>
-			<td>Final Exam: 10 - 11:50 am
+			<td>Section 2 Final Project Presentations: 3 - 4:50 pm
 			<td>
-</table>
+		<tr>
+			<td>Tue, 05/02
+			<td>
+			<td>Section 1 Final Project Presentations: 1 - 2:50 pm
+			<td>
+	</table>
