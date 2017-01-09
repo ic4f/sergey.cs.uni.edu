2ebb5d153a0532b2c5154ebb2a479455f89f1d6c
sergey <sgolitsynskiy@gmail.com>
2017-01-08 18:21:32 -0600

    comm2555 tmp

--- www/courses/comm2555/spring2017/myindex.php
+++ www/courses/comm2555/spring2017/myindex.php
@@ -1,4 +1,4 @@
-<img src="images/textbooks.jpg" alt="" 
+<img src="images/textbooks2.jpg" alt="" 
     class="float-left" style="width:130px;padding:5px;">
 
 <h3>About the Course</h3>
@@ -20,227 +20,271 @@
 </div>
 
 
-<h3>Course Schedule</h3>
-<table class="schedule days-2">
+<br style="clear:all">
+
+<h3>Schedule</h3>
+
+<table class="schedule days-3">
 	<thead>
 		<tr>
 			<th>Week
 			<th>Date
 			<th>Readings
-			<th>Topics
-			<th>Labs
-			<th>Homework
+			<th>Sessions &amp; Labs
+			<th>Assignments
 	<tbody>
 		<tr class="week">
-			<td rowspan="2">1</td>
-			<td>Tue, 08/23
+			<td rowspan="3">1
+			<td>Mon, 01/09
 			<td>
-			<td><a href="sessions/1">Session 1: Introduction to the course</a>
 			<td>
 			<td>
 		<tr>
-			<td>Thu, 08/25
+			<td>Wed, 01/11
 			<td>
-			<td>Session 2: How the Internet works, text editors and coding, file management and FTP
-			<td><a href="labs/lab1.pdf">Lab 1</a>
-			<td>Homework 1: <br><em><span style="color:maroon">due 8/24</span>, available on eLearning</em>
+			<td>
+			<td>
+		<tr>
+			<td>Fri, 01/13
+			<td>
+			<td>Lab 1
+			<td>
+		<tr class="week break">
+			<td rowspan="3" style="background:#f1f1f1;">2
+			<td>Mon, 01/16
+			<td>
+			<td colspan="2">Martin Luther King, Jr. Day
+		<tr>
+			<td>Wed, 01/18
+			<td>
+			<td>NO CLASS
+			<td>
+		<tr>
+			<td>Fri, 01/20
+			<td>
+			<td>
+			<td>
 		<tr class="week">
-			<td rowspan="2">2</td>
-			<td>Tue, 08/30
-            <td><a href="http://learn.shayhowe.com/html-css/building-your-first-web-page/">Shay, chapter 1</a> (In Practice: optional)
-			<td>Session 3: Introducing HTML and CSS
+			<td rowspan="3">3
+			<td>Mon, 01/23
+			<td>
 			<td>
 			<td>
 		<tr>
-			<td>Thu, 09/01
-            <td><a href="http://learn.shayhowe.com/html-css/getting-to-know-html/">Shay, chapter 2</a> (read up to Hyperlinks; In Practice: optional)
-			<td>Session 4: HTML: common elements; structure and semantics
-			<td><a href="labs/lab2.pdf">Lab 2</a>
+			<td>Wed, 01/25
+			<td>
+			<td>
+			<td>
+		<tr>
+			<td>Fri, 01/27
+			<td>
+			<td>Lab 2
 			<td>
 		<tr class="week">
-			<td rowspan="2">3</td>
-			<td>Tue, 09/06
-            <td><a href="http://learn.shayhowe.com/html-css/building-your-first-web-page/">Shay, chapter 1</a> (review Working with Selectors)
-                <br><a href="http://learn.shayhowe.com/html-css/getting-to-know-css/">Shay, chapter 3</a> (Common CSS Property Values, The Cascade)
-			<td>Session 5: CSS common properties: color and length; basic selectors; cascading and inheritance
-                <br><a href="http://weblab.uni.edu/sergey/week3.html">CSS exercise</a>
+			<td rowspan="3">4
+			<td>Mon, 01/30
 			<td>
-			<td><a href="homework/hw2.pdf">Homework 2</a>
+			<td>
+			<td>
 		<tr>
-			<td>Thu, 09/08
-			<td><a href="http://learn.shayhowe.com/html-css/working-with-typography/">Shay, chapter 6</a> (In Practice: optional)
-			<td>Session 6: CSS: styling text
-            <td><a href="labs/lab3.pdf">Lab 3</a>
-                <br><a href="labs/lab3.zip">lab files</a>
-            <td>
+			<td>Wed, 02/01
+			<td>
+			<td>
+			<td>
+		<tr>
+			<td>Fri, 02/03
+			<td>
+			<td>Lab 3
+			<td>
 		<tr class="week">
-			<td rowspan="2">4</td>
-			<td>Tue, 09/13
-            <td><a href="http://learn.shayhowe.com/html-css/creating-lists/">Shay, chapter 8</a> (Unordered Lists; Ordered Lists; Nesting Lists)
-                <br><a href="http://learn.shayhowe.com/html-css/organizing-data-with-tables/">Shay, chapter 11</a> (Creating a Table; Table Structure)
-			<td>Session 7: HTML structured content: lists and tables
+			<td rowspan="3">5
+			<td>Mon, 02/06
+			<td>
 			<td>
 			<td>
 		<tr>
-			<td>Thu, 09/15
-            <td><a href="http://learn.shayhowe.com/html-css/creating-lists/">Shay, chapter 8</a> (List Item Styling; Horizontally Displaying a List)
-                <br><a href="http://learn.shayhowe.com/html-css/organizing-data-with-tables/">Shay, chapter 11</a> (Table Borders; Aligning Text) 
+			<td>Wed, 02/08
+			<td>
+			<td>
+			<td>
+		<tr>
+			<td>Fri, 02/10
+			<td>
+			<td>Lab 4
+			<td>
+		<tr class="week">
+			<td rowspan="3">6
+			<td>Mon, 02/13
+			<td>
+			<td>
+			<td>
+		<tr>
+			<td>Wed, 02/15
+			<td>
+			<td>
+			<td>
+		<tr>
+			<td>Fri, 02/17
+			<td>
+			<td>Lab 5
+			<td>
+		<tr class="week">
+			<td rowspan="3">7
+			<td>Mon, 02/20
+			<td>
+			<td>
+			<td>
+		<tr>
+			<td>Wed, 02/22
+			<td>
+			<td>
+			<td>
+		<tr>
+			<td>Fri, 02/24
+			<td>
+			<td>Exam review
+			<td>
+		<tr class="week">
+			<td rowspan="3">8
+			<td>Mon, 02/27
+			<td>
+			<td>Exam 1
+			<td>
+		<tr> 
+			<td>Wed, 03/01
+			<td>
+			<td>
+			<td>
+		<tr>
+			<td>Fri, 03/03
+			<td>
+			<td>Lab 6
+			<td>
+		<tr class="week">
+			<td rowspan="3">9
+			<td>Mon, 03/06
+			<td>
+			<td>
+			<td>
+		<tr>
+			<td>Wed, 03/08
+			<td>
+			<td>
+			<td>
+		<tr>
+			<td>Fri, 03/10
+			<td>
+			<td>Lab 7
+			<td>
 
-			<td>Session 8: CSS: styling lists and tables
-            <td><a href="labs/lab4.pdf">Lab 4</a>
-			<td>
-		<tr class="week">
-			<td rowspan="2">5</td>
-			<td>Tue, 09/20
-            <td><a href="http://learn.shayhowe.com/html-css/adding-media/">Shay, chapter 9</a> (Adding Images/Video/Audio (no styling); Semantically Identifying Figures &amp; Captions) 
-                <br><a href="http://learn.shayhowe.com/html-css/getting-to-know-html/">Shay, chapter 2</a> (Creating Hyperlinks)
+		<tr class="break">
+			<td colspan="5">
+		<tr class="break week">
+			<td colspan="3" style="border-right-width:0; border-top-width:0">
+			<td colspan="2" style="text-align:left">SPRING BREAK
+		<tr class="break">
+			<td colspan="5">
 
-			<td>Session 9: HTML: media and links
-			<td>
-			<td><a href="homework/hw3.pdf">Homework 3</a>
-		<tr>
-			<td>Thu, 09/22
-			<td><a href="http://learn.shayhowe.com/html-css/opening-the-box-model/">Shay, chapter 4</a> (up to In Practice)
-			<td>Session 10: CSS box model
-            <td><a href="labs/lab5.pdf">Lab 5</a>
-			<td>
 		<tr class="week">
-			<td rowspan="2">6</td>
-			<td>Tue, 09/27
-			<td><a href="http://learn.shayhowe.com/html-css/positioning-content/">Shay, chapter 5</a> (Uniquely Positioned Elements)
-                <br><a href="http://learn.shayhowe.com/advanced-html-css/detailed-css-positioning/#position-property">Position property</a>
-                <br><a href="http://learn.shayhowe.com/advanced-html-css/detailed-css-positioning/#z-index-property">Z-index property</a>
-			<td>Session 11: CSS positioning: normal flow, relative, absolute, static
+			<td rowspan="3">10
+			<td>Mon, 03/20
+			<td>
 			<td>
 			<td>
 		<tr>
-			<td>Thu, 09/29
-            <td><a href="http://learn.shayhowe.com/html-css/positioning-content/">Shay, chapter 5</a> (Positioning with Floats)
-                <br>Optional: <a href="http://learn.shayhowe.com/advanced-html-css/detailed-css-positioning/#containing-floats">Containing floats</a> 
-			<td>Session 12: CSS floats and reusable layouts
-            <td><a href="labs/lab6.pdf">Lab 6</a>
+			<td>Wed, 03/22
 			<td>
-		<tr class="week">
-			<td rowspan="2">7</td>
-			<td>Tue, 10/04
-			<td>TBD
-			<td>Session 13: CSS gradients and transitions
-			<td>
-			<td><a href="homework/hw4.pdf">Homework 4</a>
-		<tr>
-			<td>Thu, 10/06
-			<td>
-			<td>Session 14: Exam overview
-            <td><a href="labs/lab7.pdf">Lab 7</a>
-			<td>
-		<tr class="week">
-			<td rowspan="2">8</td>
-			<td>Tue, 10/11
-			<td colspan="4">Exam-1
-		<tr>
-			<td>Thu, 10/13
-			<td><a href="http://www.nytimes.com/2006/06/26/arts/26crea.html">In Digital Age, Advancing a Flexible Copyright System</a>
-			<td>Session 15: Exam review<br>Copyright, Public Domain, &amp; the Creative Commons
-			<td>
-			<td>
-		<tr class="week">
-			<td rowspan="2">9</td>
-			<td>Tue, 10/18
-			<td>
-			<td>Session 16: Introduction to Photoshop
 			<td>
 			<td>
 		<tr>
-			<td>Thu, 10/20
-			<td>Lynda tutorials (TBD)
-			<td>Session 17: Image optimization; building an image gallery
+			<td>Fri, 03/24
+			<td>
 			<td>Lab 8
 			<td>
 		<tr class="week">
-			<td rowspan="2">10</td>
-			<td>Tue, 10/25
-			<td>Williams, pp. 11-54
-			<td>Session 18: Design principles: proximity and alignment
-                <br>Introduction to Illustrator
+			<td rowspan="3">11
+			<td>Mon, 03/27
+			<td>
 			<td>
 			<td>
 		<tr>
-			<td>Thu, 10/27
-            <td><a href="tutorials">Lynda tutorials</a>
-			<td>Photoshop selections / building a collage
-			<td>Lab 9
-			<td>Homework 5
+			<td>Wed, 03/29
+			<td>
+			<td>
+			<td>
+		<tr>
+			<td>Fri, 03/31
+			<td>
+			<td>Exam review
+			<td>
 		<tr class="week">
-			<td rowspan="2">11</td>
-			<td>Tue, 11/01
-			<td>Williams, pp. 55-94
-			<td>Session 19: Design principles: repetition and contrast
+			<td rowspan="3">12
+			<td>Mon, 04/03
+			<td>
+			<td>Exam 2
+			<td>
+		<tr>
+			<td>Wed, 04/05
+			<td>
 			<td>
 			<td>
 		<tr>
-			<td>Thu, 11/03
-            <td><a href="tutorials">Lynda tutorials</a>
-			<td>Session 20: Photoshop compositing, more selection techniques, and smart objects
+			<td>Fri, 04/07
+			<td>
+			<td>Lab 9
+			<td>
+		<tr class="week">
+			<td rowspan="3">13
+			<td>Mon, 04/10
+			<td>
+			<td>
+			<td>
+		<tr>
+			<td>Wed, 04/12
+			<td>
+			<td>
+			<td>
+		<tr>
+			<td>Fri, 04/14
+			<td>
 			<td>Lab 10
 			<td>
 		<tr class="week">
-			<td rowspan="2">12</td>
-			<td>Tue, 11/08
-			<td>Williams, pp. 95-112
-			<td>Session 21: Designing with color
+			<td rowspan="3">14
+			<td>Mon, 04/17
+			<td>
 			<td>
 			<td>
 		<tr>
-			<td>Thu, 11/10
-            <td><a href="tutorials">Lynda tutorials</a>
-			<td>Session 22: Photoshop adjustment layers; more tools as time permits
-                <br>Exam overview
+			<td>Wed, 04/19
+			<td>
+			<td>
+			<td>
+		<tr>
+			<td>Fri, 04/21
+			<td>
 			<td>Lab 11
-			<td>Homework 6
+			<td>
 		<tr class="week">
-			<td rowspan="2">13</td>
-			<td>Tue, 11/15
-			<td colspan="4">Exam-2
-		<tr>
-			<td>Thu, 11/17
+			<td rowspan="3">15
+			<td>Mon, 04/24
 			<td>
-			<td>Session 24: Exam review / project discussion
-			<td>
-			<td>
-		<tr class="break week">
-			<td rowspan="2">14</td>
-			<td>Tue, 11/22
-			<td colspan="4" rowspan="2">THANKSGIVING BREAK
-		<tr class="break">
-			<td>Thu, 11/24
-		<tr class="week">
-			<td rowspan="2">15</td>
-			<td>Tue, 11/29
-			<td>Williams, pp. 149-218
-			<td>Session 25: Typography
 			<td>
 			<td>
 		<tr>
-			<td>Thu, 12/01
-			<td>TBD
-			<td>Session 26: Project implementation: layout
-			<td>Lab 12
+			<td>Wed, 04/26
 			<td>
-		<tr class="week">
-			<td rowspan="2">16</td>
-			<td>Tue, 12/06
-			<td>TBD
-			<td>Session 27: Project implementation: editorial style 
 			<td>
 			<td>
 		<tr>
-			<td>Thu, 12/08
+			<td>Fri, 04/28
 			<td>
-			<td>Session 28: Project implementation: wrap-up
-			<td>
+			<td>Course review
 			<td>
 		<tr class="week">
-			<td>17</td>
-			<td>Thu 12/15
-			<td colspan="4">Final Project Presentation (section 2: 10:00 - 11:50 a.m. / section 3: 3:00 - 4:50 p.m.)
+			<td>16
+			<td>Mon, 05/01
+			<td>
+			<td>Final Exam: 10 - 11:50 am
+			<td>
 </table>
